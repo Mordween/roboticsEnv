b6d179f6b03593884afb6686cabd9b2ed2506184
Mordween <al.ridolfi@laposte.net>
2024-05-02 10:03:36 +0200

    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='roboticsEnv',
+    version='0.0.0.dev1',
+    description='',
+    author='Alexandre Ridolfi',
+    author_email="al.ridolfi@laposte.fr",
+    packages= ['roboticsEnv'],
+    install_requires=[
+         
+    ],
+)
